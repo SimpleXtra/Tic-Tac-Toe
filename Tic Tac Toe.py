e59198501b1f4f619cb9e50e6375c40dc713d324
SimpleXtra <41258075+SimpleXtra@users.noreply.github.com>
2021-01-26 13:27:26 +0700

    Improved code spacing and variable naming for better readability

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -29,15 +29,18 @@ class Gameplay:
 			elif DEV_MODE:
 				try:
 					if position == "": raise SyntaxError
+					
 					builtinAccess = {"print": print, "len": len, "str": str, "True": True, "False": False}
-					globalAccess = {"comp": comp, "cells": comp.cells, "autoplay": autoplay}
+					globalAccess = {"ai": ai, "cells": ai.cells, "train": train, "autoplay": train.autoplay}
 					parse = compile(position, "<string>", "eval")
 					for keyword in parse.co_names:
 						if keyword not in builtinAccess and keyword not in globalAccess:
 							raise NameError(keyword)
+					
 					eval(position, {"__builtins__": builtinAccess}, globalAccess)
 				except NameError as keyword:
 					keywords = list(dir(__builtins__) + dir(globals()) + dir(locals()) + dir(Gameplay) + dir(Computer) + list(globals().keys()) + list(locals().keys()))
+					
 					if str(keyword) in keywords:
 						print(f"Error: Access blocked to keyword '{keyword}'.")
 					else:
@@ -49,10 +52,12 @@ class Gameplay:
 					print("Error: ", message)
 			else:
 				print("Error: Invalid input.")
-			if not isAutoplay:
+			
+			if not train.isRunning:
 				self.draw(input("Your move: "), symbol)
 		else:
 			position = int(position)
+			
 			if position not in self.stats.options:
 				print("Error: Position unavailable.")
 				self.draw(input("Your move: "), symbol)
@@ -61,44 +66,37 @@ class Gameplay:
 				column = position - 1 - row * 3
 				self.board[row][column] = symbol
 				self.stats.options.remove(position)
-				comp.history += symbol + str(position)
+				ai.history += symbol + str(position)
 	
 	def check(self):
-		diagList = list()
-		diagList2 = list()
+		diagonalNWSE = list()
+		diagonalSWNE = list()
 
 		for x in range(3):
 			if (len(set(self.board[x])) == 1 and self.board[x][0] != " ") or (self.board[0][x] == self.board[1][x] == self.board[2][x] and self.board[0][x] != " "):
 				return True
+			
 			y = (2, 1, 0)
-			diagList.append(self.board[x][x])
-			diagList2.append(self.board[y[x]][x])
+			diagonalNWSE.append(self.board[x][x])
+			diagonalSWNE.append(self.board[y[x]][x])
 		
-		return (len(set(diagList)) == 1 and diagList[0] != " ") or (len(set(diagList2)) == 1 and diagList2[0] != " ")
+		return (len(set(diagonalNWSE)) == 1 and diagonalNWSE[0] != " ") or (len(set(diagonalSWNE)) == 1 and diagonalSWNE[0] != " ")
 	
 	def end(self, result: bool = None):
-		global isAutoplay
-		global progressVisible
-		if result == True:
-			if progressVisible: print("You Win!")
+		if result:
+			if train.visible: print("You Win!")
 			self.stats.score["player"] += 1
 		elif result == False:
-			if progressVisible: print("You Lose!")
+			if train.visible: print("You Lose!")
 			self.stats.score["computer"] += 1
-		elif result == None and progressVisible:
+		elif result == None and train.visible:
 			print("It's a draw!")
-		if progressVisible: print("Score:", self.stats.score["player"], "VS", self.stats.score["computer"])
-		if isAutoplay:
-			autoplay.round += 1
-			if datetime.now() - autoplay.timeSinceLastUpdate >= timedelta(seconds = 5) and not progressVisible:
-				delta = datetime.now() - autoplay.startTime
-				estTime = datetime.fromtimestamp(float(autoplay.limit / autoplay.round) * delta.total_seconds())
-				remaining = estTime - datetime.now() + autoplay.startTime
-				speed = round(autoplay.round / delta.total_seconds())
-				print("Estimated Total Time:", estTime.strftime("%M:%S.%f")[:-4])
-				print("Time Left:", remaining.strftime("%M:%S.%f")[:-4])
-				print("Speed:", speed, "games/s\n")
-				autoplay.timeSinceLastUpdate = datetime.now()
+		
+		if train.visible: print("Score:", self.stats.score["player"], "VS", self.stats.score["computer"])
+		
+		if train.isRunning:
+			train.gameNo += 1
+			train.updateTimeInfo()
 		else:
 			print("Type 'r' to retry.")
 	
@@ -118,93 +116,107 @@ class Gameplay:
 
 class Computer:
 	cells = dict()
-	
-	def __init__(self):
-		self.history = ""
-	
-	def insertNewCell(self):
-		if self.history not in self.cells.keys():
-			self.cells[self.history] = game.stats.options.copy()
+	history = ""
 
 	def analyze(self, result: bool = None):
 		if result == False:		#Computer wins
 			lastMove = int(self.history[-1])
 			self.cells[self.history[0:-2]].append(lastMove)
 		elif result:			#Computer loses
-			lastMove = int(self.history[-3])
-			trim = -4
-			prev = -3
-			self.cells[self.history[0:-4]].remove(lastMove)
-			while len(self.cells[self.history[0:trim]]) == 0:
-				trim -= 4
-				prev -= 4
-				self.cells[self.history[0:trim]].remove(int(self.history[prev]))
+			trimIndex = -4
+			lastMoveIndex = -3
+			self.cells[self.history[0:-4]].remove(int(self.history[lastMoveIndex]))
+			
+			while len(self.cells[self.history[0:trimIndex]]) == 0:
+				trimIndex -= 4
+				lastMoveIndex -= 4
+				self.cells[self.history[0:trimIndex]].remove(int(self.history[lastMoveIndex]))
 
 	def move(self):
-		self.insertNewCell()
+		if self.history not in self.cells.keys(): self.cells[self.history] = game.stats.options.copy()
 		selection = choice(self.cells[self.history])
-		if progressVisible: print("Computer's move:", selection)
+		if train.visible: print("Computer's move:", selection)
 		game.draw(selection, "O")
+	
+	class Training:
+		def __init__(self):
+			self.isRunning = False
+			self.visible = True
+		
+		def autoplay(self, iterations: int, showProgress: bool):
+			if not self.isRunning:
+				self.limit = iterations
+				self.gameNo = 0
+				self.startTime = datetime.now()
+				self.timeSinceLastUpdate = datetime.now() - timedelta(seconds = 5)
+				game.draw(choice(game.stats.options), "X")
+			
+			self.isRunning = self.gameNo < iterations
+			self.visible = showProgress if self.isRunning else True
 
-def autoplay(iterations: int, showProgress: bool):
-	global isAutoplay
-	global progressVisible
-	
-	if not isAutoplay:
-		autoplay.limit = iterations
-		autoplay.round = 0
-		autoplay.startTime = datetime.now()
-		autoplay.timeSinceLastUpdate = datetime.now() - timedelta(seconds = 5)
-		game.draw(choice(game.stats.options), "X")
-	
-	elapsed = datetime.now() - autoplay.startTime + datetime.strptime("00:00:00", "%H:%M:%S")
-	isAutoplay = autoplay.round < iterations
-	progressVisible = showProgress if autoplay.round < iterations else True
-	if autoplay.round >= iterations: print("Autoplay finished after", iterations, "games in", elapsed.strftime("%M:%S.%f")[:-4], "\n")
+			elapsed = datetime.now() - self.startTime + datetime.strptime("0", "%S")
+			if not self.isRunning: print("Autoplay finished after", iterations, "games in", elapsed.strftime("%M:%S.%f")[:-4], "\n")
+		
+		def updateTimeInfo(self):
+			if datetime.now() - self.timeSinceLastUpdate >= timedelta(seconds = 5) and not self.visible:
+				elapsedTime = (datetime.now() - self.startTime).total_seconds()
+				estimatedTime = datetime.fromtimestamp(float(self.limit / self.gameNo) * elapsedTime)
+				remaining = estimatedTime - datetime.now() + self.startTime
+				speed = round(self.gameNo / elapsedTime)
+				print("Estimated Total Time:", estimatedTime.strftime("%M:%S.%f")[:-4])
+				print("Time Left:", remaining.strftime("%M:%S.%f")[:-4])
+				print("Speed:", speed, "games/s\n")
+				self.timeSinceLastUpdate = datetime.now()
 
 game = Gameplay()
-comp = Computer()
-isAutoplay = False
-progressVisible = True
+ai = Computer()
+train = ai.Training()
 
 while True:
 	try:
 		if game.stats.playerTurn:
-			if progressVisible: print("Options:", str(game.stats.options))
-			playerMove = choice(game.stats.options) if isAutoplay else input("Your move: ")
+			if train.visible: print("Options:", str(game.stats.options))
+			playerMove = choice(game.stats.options) if train.isRunning else input("Your move: ")
 			game.draw(playerMove, "X")
 		else:
-			comp.move()
-		if progressVisible:
+			ai.move()
+		
+		if train.visible:
 			game.printBoard()
 			print()
-			if DEV_MODE: print("Past moves: ", comp.history)
+			if DEV_MODE: print("Past moves: ", ai.history)
+		
 		win = game.stats.playerTurn if game.check() else None
+		
 		if game.check() or len(game.stats.options) == 0:
 			game.end(win)
-			comp.analyze(win)
-			prompt = input() if not isAutoplay else "r"
+			ai.analyze(win)
+			prompt = input() if not train.isRunning else "r"
+			
 			if prompt == "r":
 				del game
 				game = Gameplay()
 				game.stats.changeFirstTurn(game.stats)
-				comp.history = ""
-				if progressVisible: print("#"*50)
-				if isAutoplay: autoplay(autoplay.limit, progressVisible)
+				ai.history = ""
+				if train.visible: print("#"*50)
+				if train.isRunning: train.autoplay(train.limit, train.visible)
 				continue
 			else:
 				quit()
+		
 		game.stats.changeTurn(game.stats)
 	except Exception as error:
 		print("Something went wrong. Error:", error)
+		
 		if input("Print backup data? (Yes/No)").lower() == "yes":
 			print("BACKUP DATA")
-			print("Moves history on last game:", comp.history)
+			print("Moves history on last game:", ai.history)
 			print("Last board situation:")
 			game.printBoard()
-			print("Computer's dictionary based on last history:", comp.cells.get(comp.history))
+			print("Computer's dictionary based on last history:", ai.cells.get(ai.history))
 			if input("Print full dictionary? (Yes/No)").lower() == "yes":
-				print("Computer's full dictionary:", comp.cells, sep = "\n")
+				print("Computer's full dictionary:", ai.cells, sep = "\n")
+		
 		print("Session ended.")
 		input("Press enter/return to close the application.")
 		quit()
